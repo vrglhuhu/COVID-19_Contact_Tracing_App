6e4987551b46040f95af6e12a5e6b119089d1af5
vrglhuhu <cheanbvergel@gmail.com>
2023-07-12 11:35:47 +0800

    Creating psuedocodes.

--- contact_tracing_app.py
+++ contact_tracing_app.py
@@ -1,2 +1,10 @@
 # Vergel, Chean Bernard V.
 # Covid 19 Contact Tracing App
+
+# Import tkinter
+# create add action
+# create search action
+# Create the main window
+# Set the background image
+# Create buttons for adding and searching
+# Start the main loop
