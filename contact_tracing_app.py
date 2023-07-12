ff7cb74e1772b8d96c3d16e4bd6cb3b7918ce68a
vrglhuhu <cheanbvergel@gmail.com>
2023-07-12 13:07:55 +0800

    tranferring the method to the bottom part.

--- contact_tracing_app.py
+++ contact_tracing_app.py
@@ -5,16 +5,6 @@
 import tkinter as tk
 from PIL import ImageTk, Image
 
-# Create add action
-def add_action():
-    # Add action logic goes here
-    pass
-
-# Create search action
-def search_action():
-    # Search action logic goes here
-    pass
-
 # Create the main window
 def main_window():
     window = tk.Tk()
@@ -29,18 +19,28 @@ def main_window():
     image_path = r"C:\Users\Chean vergel\Pictures\Saved Pictures\papers.co-vx94-simple-architecture-blue-pattern-background-35-3840x2160-4k-wallpaper.jpg"
     image = Image.open(image_path)
     photo = ImageTk.PhotoImage(image)
-    image_label = tk.Label(window, image=photo)
-    image_label.pack()
+    
+    background_label = tk.Label(window, image=photo)
+    background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
     # Create buttons for adding and searching
     add_button = tk.Button(window, text="Add", command=add_action)
-    add_button.place(x=150, y=100, width=100, height=30)
+    add_button.place(x=520, y=230, width=100, height=30)
 
     search_button = tk.Button(window, text="Search", command=search_action)
-    search_button.place(x=150, y=150, width=100, height=30)
+    search_button.place(x=535, y=290, width=100, height=30)
 
+    # Create add action
+    def add_action():
+        # Add action logic goes here
+        pass
+
+    # Create search action
+    def search_action():
+        # Search action logic goes here
+        pass    
+    
     # Start the main loop
     window.mainloop()
-
 # Call the main_window function to run the application
 main_window()
